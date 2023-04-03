b7a93191006a49afb6fa80ca159106fbeff8a3d0
Hamin Park <haminjp@naver.com>
2023-04-03 15:04:58 +0900

    feat: usePageLoading hook 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Loading from "@/components/Loading";
+import usePageLoading from "@/hooks/usePageLoading";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { isPageLoading } = usePageLoading();
+
+  if (isPageLoading) return <Loading />;
+  else return <Component {...pageProps} />;
 }
